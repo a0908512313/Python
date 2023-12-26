3f6ff2cce826468a2660448010165c3f1a96f4d5
a0908512313 <a0908512313@gmail.com>
2023-12-26 23:20:29 +0800

    age calculator NOT finished

--- age calculator.py
+++ age calculator.py
@@ -27,12 +27,13 @@ dateEntry.grid(column=1, row=4)
 
 def getInput():
     name = nameEntry.get()
-    monkey = Person(name, datetime.date(int(yearEntry.get())),
-                    int(monthEntry.get()), int(dateEntry.get()))
+    monkey = Person(str(nameEntry), datetime.date((int(yearEntry.get())),
+                                                  int(monthEntry.get()),
+                                                  int(dateEntry.get())))
 
     textArea = tk.Text(master=window, height=10, width=25)
     textArea.grid(column=1, row=6)
-    answer = ' Heyy {monkey}!!!. You are {age} year old!!!'.format(
+    answer = f' Heyy {monkey}!!!. You are {age} year old!!!'.format(
         monkey=monkey.name(), age=monkey.age())
 
     textArea.insert(tk.END, answer)
