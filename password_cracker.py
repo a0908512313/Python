fd15d9ba350eb196db6002a86155c0c6065a5951
a0908512313 <a0908512313@gmail.com>
2024-02-01 21:24:53 +0800

    Password Cracke half finish

--- password_cracker.py
+++ password_cracker.py
@@ -1,15 +1,13 @@
-number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-
-password = input('Please enter the password : ')
-ArrayPassword = [char for char in password]
-
-cracked = []
-
-for i in range(len(ArrayPassword)):
-    for j in range(len(number)):
-        if ArrayPassword[i] == number[j]:
-            cracked[i] = number[j]
-
-StrinCracked = ' '.join(cracked)
-
-print('Your password is : ', StrinCracked)
+from random import *
+password = input("Password: ")
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+           "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+           "y", "0" "1", "2", "3", "4", "5", "5", "6", "7", "8", "9"]
+guess = ""
+while (guess != password):
+    guess = ""
+    for i in range(len(password)):
+        guessletter = letters[randint(0, 35)]
+        guess += str(guessletter)
+    print(guess)
+print("Your password is : ")
