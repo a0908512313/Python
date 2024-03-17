9184b273a4e723fb145470ce6467cd918ca71d6c
a0908512313 <a0908512313@gmail.com>
2024-03-17 17:36:11 +0800

    Todo APP init

---
+++ Todo APP/todo app.py
@@ -0,0 +1,23 @@
+from flet import *
+
+
+def main(page: Page):
+    page.title = "Todo APP"
+    page.vertical_alignment = MainAxisAlignment.CENTER
+    page.horizontal_alignment = MainAxisAlignment.CENTER
+
+    def add_task(e):
+        page.add(Checkbox(label=new_task_value))
+        new_task_value = ''
+        page.update()
+
+    new_task = TextField(hint_text="Please enter the task")
+
+    page.add(new_task,
+             FloatingActionButton(
+                 icon=icons.ADD,
+                 on_click=add_task)
+             )
+
+
+app(target=main)
