fa0b1c22dbdac4d667ead26a02c656d64b418616
Pear <a0908512313@gmail.com>
2024-09-08 18:43:13 +0800

    fix

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 n = input("")
 start = "AUG"
-end = ["UAA","UAG","UGA"]
+end = ("UAA", "UAG", "UGA")
 result = 0
-for i in range(n-3):
+i = 0
+while i <= len(n) - 3:
     temp = str()
-    temp+=n[i]+n[i+1]+n[i+2]
+    temp += n[i]+n[i+1]+n[i+2]
     if temp == start:
         result += 1
         i += 3
-        while temp != start:
+        temp += n[i]+n[i+1]+n[i+2]
+        while temp not in end:
             i += 3
-            temp+=n[i]+n[i+1]+n[i+2]
-            for j in end:
-                if temp == j:
-                    break
+            temp += n[i]+n[i+1]+n[i+2]
+            result += 1
+    else:
+        i += 1
 print(result)
