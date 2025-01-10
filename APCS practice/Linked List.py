e8923d5875d46946ce9e3b661d73bdf5e9d1df60
a0908512313 <a0908512313@gmail.com>
2025-01-10 10:20:26 +0800

    Linked List

--- APCS practice/Linked List.py
+++ APCS practice/Linked List.py
@@ -12,4 +12,34 @@ class LinkList():
         if not self.head:
             self.head = Node(data)
         else:
-            pass
+            while self.head.next:
+                self.head = self.head.next
+            self.head.next = Node(data)
+
+    def insert(self, n0, P):
+        n1 = n0.next
+        P.next = n1
+        n0.next = P
+
+    def remove(self, n0):
+        if not n0.next:
+            return
+        P = n0.next
+        n1 = P.next
+        n0.next = n1
+
+    def access(self, head, index):
+        for _ in range(index):
+            if not head:
+                return None
+            head = head.next
+        return head
+
+    def find(self, head, target):
+        index = 0
+        while head:
+            if head.val == target:
+                return index
+            head = head.next
+            index += 1
+        return None
